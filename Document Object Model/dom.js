be5f8ae21d5f3d6e0244bc2c32176fc7e109a361
Aparna <aparna6119@gmail.com>
2024-10-21 23:56:05 +0530

    DOM

---
+++ Document Object Model/dom.js
@@ -0,0 +1,12 @@
+//WINDOW OBJECT
+
+//The window object represents an open window in a browser. it is browser's object (not javascripts's) & is automatically created by browser.
+
+//it is a global object with lots  of properties & methods
+console.log("hello1");
+window.console.log("hello2");
+window.alert("attention!");
+
+
+
+//DOM====== DOCUMENT OBJECT MODEL
